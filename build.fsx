1c507c7487ed39413df8be53da55a91960cbdd43
Andrew Meier <ameier38@gmail.com>
2020-11-27 17:57:04 -0500

    initial commit

---
+++ build.fsx
@@ -0,0 +1,22 @@
+#load ".fake/build.fsx/intellisense.fsx"
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open BlackFox.Fake
+
+let clean = BuildTask.create "Clean" [] {
+    !! "src/**/bin"
+    ++ "src/**/obj"
+    |> Shell.cleanDirs 
+}
+
+let build = BuildTask.create "Build" [clean.IfNeeded] {
+    !! "src/**/*.*proj"
+    |> Seq.iter (DotNet.build id)
+}
+
+let _all = BuildTask.createEmpty "All" [clean; build]
+
+BuildTask.runOrDefault _all
